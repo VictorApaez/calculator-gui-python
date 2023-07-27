a3df096a9bb2a4a765b3bbc9671e5d58f2707cb9
VictorApaez <vic.apaez@gmail.com>
2023-07-27 16:20:08 -0400

    crated calculator class based on tests

--- src/calculator.py
+++ src/calculator.py
@@ -1,4 +1,25 @@
 class Calculator:
   def __init__(self):
-      self.initial_val = 0
+    self.current_value = 0
 
+  def add(self, value):
+    self.current_value += value
+    return self
+
+  def subtract(self, value):
+    self.current_value -= value
+    return self
+
+  def multiply(self, value):
+    self.current_value *= value
+    return self
+
+  def divide(self, value):
+    if value != 0:
+      self.current_value /= value
+      return self
+    else:
+      raise ValueError("Cannot Divide by Zero")
+      
+  def result(self):
+    return self.current_value
